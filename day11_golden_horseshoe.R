b3f2f524381ab3910bcc30b1589ae3ff9022cb2c
eugejoh <ejoh@bu.edu>
2019-11-12 10:25:48 -0500

    added crop and tinkered with plot_3d params

--- day11_golden_horseshoe.R
+++ day11_golden_horseshoe.R
@@ -20,12 +20,34 @@ library(rayshader)
 # Import
 file_path <- file.path("~", "Documents", "on_elevation_20191111", "DEM.tif")
 d <- raster::raster(file_path)
+d
+
+image(d)
+plot()
+
+ex <- raster::extent(c(-8917165, -8860000, 5312801, 5400000))
+
+d2 <- raster::crop(d, ex)
 
 # convert to matrix
-dm <- matrix(
-  raster::extract(d, raster::extent(d), buffer = 1000), 
-  nrow = ncol(d), ncol = nrow(d)
-)
+to_mat <- function(ras) {
+  matrix(
+    raster::extract(ras, raster::extent(ras), buffer = 1000), 
+    nrow = ncol(ras), ncol = nrow(ras)
+  )
+}
+
+dm2 <- to_mat(d2)
+
+
+dm2 %>% 
+  sphere_shade(texture = "imhof1") %>%
+  add_shadow(shadowmap = ray_shade(dm2, zscale = 20)) %>% 
+  add_water(detect_water(dm2)) %>% 
+  plot_3d(dm2, zscale = 20, fov = 0, theta = 135, zoom = 0.75, phi = 45, windowsize = c(1000, 800))
+Sys.sleep(0.2)
+render_snapshot("test2", clear=TRUE)
+
 
 # rayshade away!
 ambmat <- ambient_shade(dm, zscale = 30)
